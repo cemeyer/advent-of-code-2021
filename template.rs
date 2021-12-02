a17048aa9611c01d15a7143c0d8606cfa55c1a2f
Conrad Meyer <cem@FreeBSD.org>
2021-12-01 17:08:02 -0800

    Very rough sketch of a Rust puzzle template (w/o automated submit)

---
+++ template.rs
@@ -0,0 +1,21 @@
+#![allow(dead_code, unused_imports, unused_variables)]
+
+use anyhow::{anyhow, Result};
+
+fn part1(input: &str) -> String {
+    format!("{}", XXX);
+}
+
+fn part2(input: &str) -> String {
+    format!("{}", XXX);
+}
+
+fn main() -> Result<()> {
+    let mut puzzle = aoc::Puzzle::new2021(XXX)?;
+    let data = puzzle.get_data()?;
+
+    println!(part1(data));
+    //println!(part2(data));
+
+    Ok(())
+}
