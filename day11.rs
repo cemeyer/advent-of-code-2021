f9d1bf518548f6a8e786098d32a7b4f897e99e9b
Conrad Meyer <cem@FreeBSD.org>
2021-12-10 21:23:43 -0800

    Day 11 solutions
    The only tricky thing here is executing fast and making sure you follow
    all of the puzzle's instructions -- e.g., squares only flash once per
    step, and all flashed squares reset to zero at the end of the step.
    Sometimes problems like this require a copied input (so I copied step to
    step) but it looks like this one could have been done in-place.
    
    Probably should have just used grid_2d_graph for the neighbors
    interaction instead of the open-coded dy,dc loop -- it's somewhat less
    painful in Python.

---
+++ day11.rs
@@ -0,0 +1,119 @@
+#![allow(dead_code, unused_assignments, unused_imports, unused_parens, unused_variables)]
+
+use anyhow::{anyhow, Result};
+use graphlib::{Graph, VertexId};
+use ndarray::prelude::*;
+use ndarray::{ArcArray2, parallel::par_azip};
+use std::cmp::{min, max};
+use std::convert::{TryFrom, TryInto};
+use std::collections::*;
+use std::hash::Hash;
+use std::iter::FromIterator;
+
+fn step(grid: &Vec<Vec<u8>>) -> (Vec<Vec<u8>>, HashSet<(usize, usize)>) {
+    let mut newgrid = grid.clone();
+    let mut flashes = HashSet::new();
+
+    for (r, row) in grid.iter().enumerate() {
+        for (c, octopus) in row.iter().enumerate() {
+            newgrid[r][c] = *octopus + 1;
+        }
+    }
+
+    let mut any = true;
+    while any {
+        any = false;
+        for r in 0..newgrid.len() {
+            for c in 0..newgrid[0].len() {
+                let octo = newgrid[r][c];
+                if octo > 9 && !flashes.contains(&(r, c)) {
+                    any = true;
+                    flashes.insert((r, c));
+                    for dr in -1i64..=1 {
+                        let rr = r as i64 + dr;
+                        if rr < 0 || rr >= newgrid.len() as i64 {
+                            continue;
+                        }
+                        for dc in -1i64..=1 {
+                            let cc = c as i64 +dc;
+                            if cc < 0 || cc >= newgrid[0].len() as i64 {
+                                continue;
+                            }
+                            if dc == 0 && dr == 0 {
+                                continue;
+                            }
+                            newgrid[rr as usize][cc as usize] += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (r, row) in newgrid.iter_mut().enumerate() {
+        for (c, octo) in row.iter_mut().enumerate() {
+            if *octo > 9 {
+                *octo = 0;
+            }
+        }
+    }
+
+    (newgrid, flashes)
+}
+
+fn part1(input: &Vec<Vec<u8>>) -> usize {
+    let mut grid = input.clone();
+    let mut totalflash = 0;
+    for _ in 0..100 {
+        let (newgrid, flashes) = step(&grid);
+        grid = newgrid;
+        totalflash += flashes.len();
+    }
+
+    totalflash
+}
+
+fn part2(input: &Vec<Vec<u8>>) -> u64 {
+    let mut grid = input.clone();
+    let mut n = 0;
+    loop {
+        let (newgrid, flashes) = step(&grid);
+        grid = newgrid;
+        n += 1;
+        if flashes.len() == 100 {
+            break;
+        }
+    }
+
+    n
+}
+
+fn main() -> Result<()> {
+    let mut puzzle = aoc::Puzzle::new(2021, 11)?;
+    let data = puzzle.get_data()?;
+//    let data =
+//"5483143223
+//2745854711
+//5264556173
+//6141336146
+//6357385478
+//4167524645
+//2176841721
+//6882881134
+//4846848554
+//5283751526";
+    let lines = data.lines().collect::<Vec<_>>();
+    let grid = lines.iter().map(|line| {
+        line.chars().map(|c| c.to_digit(10).unwrap() as u8).collect::<Vec<_>>()
+    }).collect();
+
+    //let answ1 = part1(&grid);
+    //dbg!(&answ1);
+    //puzzle.submit_answer(aoc::Part::One, &format!("{}", answ1))?;
+
+    let answ2 = part2(&grid);
+    dbg!(&answ2);
+    //puzzle.submit_answer(aoc::Part::Two, &format!("{}", answ2))?;
+
+    Ok(())
+}
